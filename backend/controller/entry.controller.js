62cbbfbd8348528b34df8230d78be000193b093f
Eleanor Wright <elliewright055@gmail.com>
2025-03-22 05:18:27 -0500

    Add updateEntry endpoint to Entry controller

--- backend/controller/entry.controller.js
+++ backend/controller/entry.controller.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 import Entry from "../model/entry.model.js";
 import { ObjectId } from "mongodb";
 
+// TODO: Store status codes in object, i.e.
+// const httpStatusCodes = {
+//      OK: 200
+// } etc...
+
 export async function createNewEntry(req, res) {
     const newEntry = new Entry(req.body);
 
@@ -39,6 +44,40 @@ export async function getAllEntries(req, res) {
         res.status(500).json({
             success: false,
             message: "Server error in fetching all entries"
+        });
+    };
+};
+
+export async function updateEntry(req, res) {
+    const { id } = req.params;
+    const updatedEntry = req.body;
+
+    if (!updatedEntry.name) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide a name for the updated entry."
+        });
+    };
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide a valid ID to update."
+        });
+    };
+
+    try {
+        const entryToUpdate = await Entry.findByIdAndUpdate(id, updatedEntry, { new: true });
+        return res.status(200).json({
+            success: true,
+            message: "Entry updated successfully.",
+            body: entryToUpdate
+        });
+    } catch (error) {
+        console.error(`Error in updating entry: ${error.message}`);
+        return res.status(500).json({
+            success: false,
+            message: `Error in updating entry: ${error.message}`
         });
     };
 };
--- backend/route/entry.route.js
+++ backend/route/entry.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createNewEntry, deleteEntry, getAllEntries } from "../controller/entry.controller.js";
+import { createNewEntry, getAllEntries, updateEntry, deleteEntry } from "../controller/entry.controller.js";
 
 const router = express.Router();
 
 router.post("/", createNewEntry);
 router.get("/", getAllEntries);
+router.put("/:id", updateEntry);
 router.delete("/:id", deleteEntry);
 
 export default router;
