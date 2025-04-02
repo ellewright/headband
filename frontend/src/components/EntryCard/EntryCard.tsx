b93b5714bb2920c5fa052b1e8331c2c17e955672
Eleanor Wright <elliewright055@gmail.com>
2025-04-02 12:37:33 -0500

    Use Open Library Covers API to render covers by ISBN in EntryCard component

--- frontend/src/components/EntryCard/EntryCard.tsx
+++ frontend/src/components/EntryCard/EntryCard.tsx
@@ -1,14 +1,32 @@
-import { Button, Paper, Typography } from "@mui/material";
+import { Button, Container, Paper, Typography } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Entry } from "../../interfaces/Entry";
 import { deleteEntryFromDatabase } from "../../api/config";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Edit } from "@mui/icons-material";
 import UpdateEntryForm from "../UpdateEntryForm/UpdateEntryForm";
 import { JSX } from "@emotion/react/jsx-runtime";
 
 export default function EntryCard({ _id, isbn, title, author, genre, publicationYear, review, onDelete }: Entry): JSX.Element {
     const [isUpdating, setIsUpdating] = useState<boolean>(false);
+    const [isImageFound, setIsImageFound] = useState<boolean>(true);
+
+    const coverUrl = `https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`;
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = coverUrl;
+
+        img.onload = () => {
+            if (img.naturalWidth === 1) { // image not found
+                setIsImageFound(false);
+            }
+        };
+
+        img.onerror = () => { // failed to load image
+            setIsImageFound(false);
+        };
+    }, [coverUrl]);
 
     async function handleDelete(id: string) {
         try {
@@ -19,6 +37,11 @@ export default function EntryCard({ _id, isbn, title, author, genre, publication
         };
     };
 
+    function handleError() {
+        console.log("Error")
+        setIsImageFound(false);
+    };
+
     if (!isUpdating) {
         return (
             <Paper
@@ -27,22 +50,31 @@ export default function EntryCard({ _id, isbn, title, author, genre, publication
                     p: 2
                 }}
             >
+                {isImageFound && (
+                    <Container>
+                        <img
+                            src={coverUrl}
+                            alt="Cover."
+                            onError={handleError}
+                        />
+                    </Container>
+                )}
                 <Typography
                     variant="h6"
                 >
                     {title}
                 </Typography>
                 <Typography>
-                    {author}
+                    By: {author}
                 </Typography>
                 <Typography>
-                    {isbn}
+                    ISBN: {isbn}
                 </Typography>
                 <Typography>
-                    {genre}
+                    Genre: {genre}
                 </Typography>
                 <Typography>
-                    {publicationYear}
+                    Year of publication: {publicationYear}
                 </Typography>
                 <Typography
                     sx={{
